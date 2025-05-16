b4ecb87a2e37d929670110ffac189168dfd65427
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-05-16 09:58:58 +0200

    zprehledneni, cteni souboru z GitHub

--- n2k_config.R
+++ n2k_config.R
@@ -46,7 +46,7 @@ CIS_CILMON <- read.csv("cil_mon_zdroj.csv", fileEncoding = "Windows-1250")
 current_year <- 2024
 
 # LIMITY ----
-limity_cev <- read.csv("limity_cevky_mv7.csv", fileEncoding = "Windows-1250")
+limity_cev <- read.csv("limity_cevky.csv", fileEncoding = "Windows-1250")
 limity <- read.csv(
   "limity_vse.csv", fileEncoding = "Windows-1250") %>%
   dplyr::bind_rows(., limity_cev) %>%
@@ -60,18 +60,30 @@ limity <- read.csv(
   ) %>%
   dplyr::ungroup() %>%
   dplyr::group_by(DRUH, ID_IND) %>%
-  dplyr::mutate(LIM_INDLIST = toString(na.omit(unique(LIM_INDLIST)))) %>%
+  dplyr::mutate(
+    LIM_INDLIST = toString(na.omit(unique(LIM_INDLIST)))
+    ) %>%
   dplyr::ungroup()
 
-indikatory_id <- read.csv("cis_indikatory_popis.csv", fileEncoding = "Windows-1250")
+indikatory_id <- read.csv(
+  "https://raw.githubusercontent.com/jonasgaigr/host_naturecz/main/cis_indikatory_popis.csv", 
+  fileEncoding = "Windows-1250")
 
-cis_evd_perioda <- read.csv("cis_evd_perioda.csv", fileEncoding = "Windows-1250") %>%
-  dplyr::select(TAXON, PERIODA)
+cis_evd_perioda <- read.csv("https://raw.githubusercontent.com/jonasgaigr/host_naturecz/main/cis_evd_perioda.csv", 
+                            fileEncoding = "Windows-1250"
+                            ) %>%
+  dplyr::select(
+    TAXON, 
+    PERIODA
+    )
 
 # LOKALITY ----
 rn2kcz::load_n2k_sites() 
 
-n2k_oop <- readr::read_csv2("n2k_oop_25.csv", locale = readr::locale(encoding = "Windows-1250")) %>%
+n2k_oop <- readr::read_csv2(
+  "https://raw.githubusercontent.com/jonasgaigr/host_naturecz/main/n2k_oop_25.csv", 
+  locale = readr::locale(encoding = "Windows-1250")
+  ) %>%
   mutate(oop = gsub(";", ",", oop)) %>%
   dplyr::rename(SITECODE = sitecode) %>%
   dplyr::select(SITECODE, oop)
@@ -104,18 +116,38 @@ po <- sf::st_read(getfeature_url_po) %>%
 biotop_zvld <- sf::st_read(getfeature_url_biotopzvld) %>%
   sf::st_transform(., st_crs("+init=epsg:5514"))
 
-n2k_union <- sf::st_join(evl, po)
+n2k_union <- sf::st_join(
+  evl, 
+  po
+  )
 
-rp_code <- base::read.csv2("n2k_rp_25.csv", fileEncoding = "Windows-1250") %>%
-  dplyr::rename(kod_chu = sitecode) %>%
-  dplyr::select(kod_chu, pracoviste) %>%
-  dplyr::mutate(pracoviste = gsub(",", "", pracoviste))
+rp_code <- read.csv2(
+  "n2k_rp_25.csv", 
+  fileEncoding = "Windows-1250"
+  ) %>%
+  dplyr::rename(
+    kod_chu = sitecode
+    ) %>%
+  dplyr::select(
+    kod_chu, 
+    pracoviste) %>%
+  dplyr::mutate(
+    pracoviste = gsub(",", 
+                      "", 
+                      pracoviste
+                      )
+    )
 
 # BIOTOP EVD HMYZ ----
-biotop_evd <- readr::read_csv("https://raw.githubusercontent.com/jonasgaigr/N2K.CZ/main/biotopy_evd.csv")
+biotop_evd <- readr::read_csv(
+  "https://raw.githubusercontent.com/jonasgaigr/host_naturecz/main/biotopy_evd_hmyz.csv"
+  )
 
 # NDOP EXPORT ----
-n2k_export <- readr::read_csv2("C:/Users/jonas.gaigr/Documents/host_data/evl_data_export_20250408.csv", locale = readr::locale(encoding = "Windows-1250"))
+n2k_export <- readr::read_csv2(
+  "C:/Users/jonas.gaigr/Documents/host_data/evl_data_export_20250408.csv", 
+  locale = readr::locale(encoding = "Windows-1250")
+  )
 #volna_export <- readr::read_csv("volna_data_export_20250317.csv", locale = readr::locale(encoding = "Windows-1250"))
 #n2k_export <- bind_rows(n2k_export, volna_export)
 
